ff12b34d4d3788fbdd98d950d6874b85a4349888
Nicklas Nygren <hello@nickl.as>
2014-12-28 20:25:10 +0100

    Broke out assertion helper functions to separate file

--- test/app.js
+++ test/app.js
@@ -1,38 +1,12 @@
-var app     = require('..').app;
-var pywws   = require('..').pywws;
-var routes  = require('..').routes;
-var request = require('supertest');
-var should  = require('should');
-var async   = require('async');
-
-var assertMultiItemResponse = function (expectEmpty) {
-  var doneCallback = arguments[arguments.length-1];
-
-  return function (err, response) {
-    response.body.data.should.be.an.Array;
-    if (!expectEmpty) {
-      response.body.data[0].idx.should.be.ok;
-      response.body.data.length.should.be.greaterThan(0);
-    }
-
-    if (doneCallback) doneCallback();
-  };
-};
-
-var assertSingleItemResponse = function () {
-  var doneCallback = arguments[arguments.length-1];
-
-  return function (err, response) {
-    response.body.data.should.be.an.Object;
-    response.body.data.idx.should.be.ok;
-    response.body.data.should.not.be.an.Array;
-
-    if (doneCallback) doneCallback();
-  };
-};
+var app       = require('..').app;
+var assert    = require('./support/assert');
+var async     = require('async');
+var pywws     = require('..').pywws;
+var request   = require('supertest');
+var routes    = require('..').routes;
+var should    = require('should');
 
 describe('API:', function () {
-
   it('should have set a pywws root', function () {
     app.get('pywws_root').should.be.a.String.and.not.be.empty;
   });
@@ -70,13 +44,13 @@ describe('API:', function () {
       it(method + ' should get data range', function (done) {
         request(app).
           get('/' + method + '/' + d1 + '/' + d2).
-          expect(200, assertMultiItemResponse(done));
+          expect(200, assert.multiItemResponse(done));
       });
 
       it(method + ' should get single data point', function (done) {
         request(app).
           get('/' + method + '/' + d1).
-          expect(200, assertSingleItemResponse(done));
+          expect(200, assert.singleItemResponse(done));
       });
 
       callback();
---
+++ test/support/assert.js
@@ -0,0 +1,31 @@
+var assertMultiItemResponse = function (expectEmpty) {
+  var doneCallback = arguments[arguments.length-1];
+
+  return function (err, response) {
+    response.body.data.should.be.an.Array;
+    if (!expectEmpty) {
+      response.body.data[0].idx.should.be.ok;
+      response.body.data.length.should.be.greaterThan(0);
+    }
+
+    if (doneCallback) doneCallback();
+  };
+};
+
+var assertSingleItemResponse = function () {
+  var doneCallback = arguments[arguments.length-1];
+
+  return function (err, response) {
+    response.body.data.should.be.an.Object;
+    response.body.data.idx.should.be.ok;
+    response.body.data.should.not.be.an.Array;
+
+    if (doneCallback) doneCallback();
+  };
+};
+
+module.exports = {
+  multiItemResponse:  assertMultiItemResponse,
+  singleItemResponse: assertSingleItemResponse,
+};
+
